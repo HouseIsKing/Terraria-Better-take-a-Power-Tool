8e922780d394f85c469eff12c961f2c6f8bd499f
dennisw100 <100dennisw@gmail.com>
2017-11-10 07:29:21 +0100

    Add files via upload

---
+++ Items/WoodenJackhammer.cs
@@ -0,0 +1,50 @@
+using Terraria.ID;
+
+using Terraria.ModLoader;
+
+
+
+namespace BettertakeaPowerTool.Items
+
+{
+
+	public class WoodenJackhammer : ModItem
+
+	{
+
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Wooden Jackhammer");
+		}
+		public override void SetDefaults()
+
+		{
+
+			item.CloneDefaults(ItemID.WoodenHammer);
+			item.channel = true;
+			item.noUseGraphic = true;
+			item.noMelee = true;
+			item.useStyle = 5;
+			item.UseSound = SoundID.Item23;
+			item.shoot = mod.ProjectileType("WoodenJackhammer");
+			item.shootSpeed = 40f;
+		}
+
+
+		public override void AddRecipes()
+
+		{
+
+			ModRecipe recipe = new ModRecipe(mod);
+			recipe.AddIngredient(ItemID.Wood, 8);
+			recipe.AddTile(TileID.WorkBenches);
+
+			recipe.SetResult(this);
+
+			recipe.AddRecipe();
+
+		}
+
+	}
+
+}
