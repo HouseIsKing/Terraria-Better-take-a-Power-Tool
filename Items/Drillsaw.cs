ea23f71ac42017c79c3d0a3b8647847c3c9dadef
dennisw100 <100dennisw@gmail.com>
2017-12-07 01:58:01 +0100

    Fix the chines translation of the Drillsaw Tooltips

--- Items/Drillsaw.cs
+++ Items/Drillsaw.cs
@@ -15,7 +15,7 @@ namespace BettertakeaPowerTool.Items
 			Tooltip.AddTranslation(GameCulture.French, "Peut extraire des briques de Lihzhard\nÀ ne pas confondre avec une forche");
 			Tooltip.AddTranslation(GameCulture.Spanish, "Capaz de extraer ladrillos de lihzahrd\nNo se debe confundir con un taladro hacha");
 			Tooltip.AddTranslation(GameCulture.Russian, "Может добывать кирпичи ящеров\nHе путать c Mолотобур");
-			Tooltip.AddTranslation(GameCulture.Chinese, "可开采丛林蜥蜴砖\n“不要”");
+			Tooltip.AddTranslation(GameCulture.Chinese, "可开采丛林蜥蜴砖\n“不要斧钻混淆”");
 			Tooltip.AddTranslation(GameCulture.Portuguese, "Capaz de minerar tijolos lagharto\n'Não confundir com uma Drax'");
 			Tooltip.AddTranslation(GameCulture.Polish, "Może kopać jaszczuhrze cegły\n„Nie mylić z Świdrolof“");
 		}
