84df954fbdfe462ce822bd53353be245bcb0afc2
dennisw100 <100dennisw@gmail.com>
2018-08-11 23:38:19 +0200

    Add files via upload

--- NPCDrops.cs
+++ NPCDrops.cs
@@ -4,43 +4,43 @@ using Terraria.ModLoader;
 
 namespace BettertakeaPowerTool
 {
-	public class NPCDrops : GlobalNPC
-	{
+    public class NPCDrops : GlobalNPC
+    {
         public override void SetupShop(int type, Chest shop, ref int nextSlot)
-		{//adds "Jackhamshroom" Item to Truffle Shop.
-			if (type == NPCID.Truffle && NPC.downedMechBossAny)
-			{
-				shop.item[nextSlot].SetDefaults(mod.ItemType("Jackhamshroom"));
-				nextSlot++;
-			}
+        {//adds "Jackhamshroom" Item to Truffle Shop.
+            if (type == NPCID.Truffle && NPC.downedMechBossAny)
+            {
+                shop.item[nextSlot].SetDefaults(mod.ItemType("Jackhamshroom"));
+                nextSlot++;
+            }
             Mod tremor = ModLoader.GetMod("Tremor");
             if (tremor != null && type == tremor.NPCType("Chef"))
             {
-                    shop.item[nextSlot].SetDefaults(mod.ItemType("PizzaCutter"));
-                    nextSlot++;
+                shop.item[nextSlot].SetDefaults(mod.ItemType("PizzaCutter"));
+                nextSlot++;
             }
         }
-		public override bool PreNPCLoot(NPC npc)
-		{
-			if (npc.type == NPCID.UndeadMiner)
-			{//decides whether undeadminer will drop BoneDrill or BonePickaxe if he will even drop.
-				if (Main.rand.Next(50) == 0)
-				{
-					if (Main.rand.Next(2) == 0)
-					{
-						Item.NewItem((int)npc.position.X, (int)npc.position.Y, npc.width, npc.height, mod.ItemType("BoneDrill"));
-					}
-					else
-					{
-						Item.NewItem((int)npc.position.X, (int)npc.position.Y, npc.width, npc.height, ItemID.BonePickaxe);
-					}
-				}
-				NPCLoader.blockLoot.Add(ItemID.BonePickaxe);
-			}
-            if (npc.type == NPCID.Golem && !Main.expertMode) 
-			{//decides what golem will drop if not in expert mode.
-				int num26 = Main.rand.Next(8);
-                switch(num26)
+        public override bool PreNPCLoot(NPC npc)
+        {
+            if (npc.type == NPCID.UndeadMiner)
+            {//decides whether undeadminer will drop BoneDrill or BonePickaxe if he will even drop.
+                if (Main.rand.Next(50) == 0)
+                {
+                    if (Main.rand.Next(2) == 0)
+                    {
+                        Item.NewItem((int)npc.position.X, (int)npc.position.Y, npc.width, npc.height, mod.ItemType("BoneDrill"));
+                    }
+                    else
+                    {
+                        Item.NewItem((int)npc.position.X, (int)npc.position.Y, npc.width, npc.height, ItemID.BonePickaxe);
+                    }
+                }
+                NPCLoader.blockLoot.Add(ItemID.BonePickaxe);
+            }
+            if (npc.type == NPCID.Golem && !Main.expertMode)
+            {//decides what golem will drop if not in expert mode.
+                int num26 = Main.rand.Next(8);
+                switch (num26)
                 {
                     case 0:
                         {
@@ -92,7 +92,7 @@ namespace BettertakeaPowerTool
                         }
                     default:
                         break;
-				}
+                }
                 NPCLoader.blockLoot.Add(ItemID.PossessedHatchet);
                 NPCLoader.blockLoot.Add(ItemID.Stynger);
                 NPCLoader.blockLoot.Add(ItemID.StyngerBolt);
@@ -103,51 +103,92 @@ namespace BettertakeaPowerTool
                 NPCLoader.blockLoot.Add(ItemID.EyeoftheGolem);
                 NPCLoader.blockLoot.Add(ItemID.SunStone);
             }
-            if (npc.type == NPCID.WallofFlesh && !Main.expertMode) 
-			{//decides what wall flash should drop if not on expert mode.
-				if (Main.rand.Next(2) == 0)
-				{
-					Item.NewItem((int)npc.position.X, (int)npc.position.Y, npc.width, npc.height, mod.ItemType("Pwnjackhammer"));
-				}
-				else
-				{
-					Item.NewItem((int)npc.position.X, (int)npc.position.Y, npc.width, npc.height, ItemID.Pwnhammer);
-				}
-				NPCLoader.blockLoot.Add(ItemID.Pwnhammer);
-			}
-            if (npc.type == NPCID.Plantera && !Main.expertMode) 
-			{//Decides what plantera should drop if not in expert mode.
-				if (Main.rand.Next(50) == 0)
-				{
-					if (Main.rand.Next(2) == 0)
-					{
-						Item.NewItem((int)npc.position.X, (int)npc.position.Y, npc.width, npc.height, mod.ItemType("TheSaw"));
-					}
-					else
-					{
-						Item.NewItem((int)npc.position.X, (int)npc.position.Y, npc.width, npc.height, ItemID.TheAxe);
-					}
-				}
-				NPCLoader.blockLoot.Add(ItemID.TheAxe);
-			}
-
-			Mod tremor = ModLoader.GetMod("Tremor");
+            if (npc.type == NPCID.WallofFlesh && !Main.expertMode)
+            {//decides what wall flash should drop if not on expert mode.
+                if (Main.rand.Next(2) == 0)
+                {
+                    Item.NewItem((int)npc.position.X, (int)npc.position.Y, npc.width, npc.height, mod.ItemType("Pwnjackhammer"));
+                }
+                else
+                {
+                    Item.NewItem((int)npc.position.X, (int)npc.position.Y, npc.width, npc.height, ItemID.Pwnhammer);
+                }
+                NPCLoader.blockLoot.Add(ItemID.Pwnhammer);
+            }
+            if (npc.type == NPCID.Plantera && !Main.expertMode)
+            {//Decides what plantera should drop if not in expert mode.
+                if (Main.rand.Next(50) == 0)
+                {
+                    if (Main.rand.Next(2) == 0)
+                    {
+                        Item.NewItem((int)npc.position.X, (int)npc.position.Y, npc.width, npc.height, mod.ItemType("TheSaw"));
+                    }
+                    else
+                    {
+                        Item.NewItem((int)npc.position.X, (int)npc.position.Y, npc.width, npc.height, ItemID.TheAxe);
+                    }
+                }
+                NPCLoader.blockLoot.Add(ItemID.TheAxe);
+            }
+            Mod tremor = ModLoader.GetMod("Tremor");
+            if (tremor != null && npc.type == NPCID.UndeadViking)
+            {
+                if (Main.rand.Next(32) == 0)
+                {
+                    if (Main.rand.Next(2) == 0)
+                    {
+                        Item.NewItem((int)npc.position.X, (int)npc.position.Y, npc.width, npc.height, mod.ItemType("NorthChainsaw"));
+                    }
+                    else
+                    {
+                        Item.NewItem((int)npc.position.X, (int)npc.position.Y, npc.width, npc.height, ModLoader.GetMod("Tremor").ItemType("NorthAxe"));
+                    }
+                }
+                NPCLoader.blockLoot.Add(ModLoader.GetMod("Tremor").ItemType("NorthAxe"));
+                if (Main.rand.Next(32) == 0)
+                {
+                    if (Main.rand.Next(2) == 0)
+                    {
+                        Item.NewItem((int)npc.position.X, (int)npc.position.Y, npc.width, npc.height, mod.ItemType("NorthJackhammer"));
+                    }
+                    else
+                    {
+                        Item.NewItem((int)npc.position.X, (int)npc.position.Y, npc.width, npc.height, ModLoader.GetMod("Tremor").ItemType("NorthHammer"));
+                    }
+                }
+                NPCLoader.blockLoot.Add(ModLoader.GetMod("Tremor").ItemType("NorthHammer"));
+            }
             if (tremor != null && npc.type == tremor.NPCType("TikiTotem") && !Main.expertMode)
-            { 
-				if (Main.rand.Next(3) == 0)
-				{
-					if (Main.rand.Next(2) == 0)
-					{
-						Item.NewItem((int)npc.position.X, (int)npc.position.Y, npc.width, npc.height, mod.ItemType("DrillofBloom"));
-					}
-					else
-					{
-						Item.NewItem((int)npc.position.X, (int)npc.position.Y, npc.width, npc.height, ModLoader.GetMod("Tremor").ItemType("PickaxeofBloom"));
-					}
-				}
-				NPCLoader.blockLoot.Add(ModLoader.GetMod("Tremor").ItemType("PickaxeofBloom"));
-			}
-			return true;
-		}
-	}
+            {
+                if (Main.rand.Next(3) == 0)
+                {
+                    if (Main.rand.Next(2) == 0)
+                    {
+                        Item.NewItem((int)npc.position.X, (int)npc.position.Y, npc.width, npc.height, mod.ItemType("DrillofBloom"));
+                    }
+                    else
+                    {
+                        Item.NewItem((int)npc.position.X, (int)npc.position.Y, npc.width, npc.height, ModLoader.GetMod("Tremor").ItemType("PickaxeofBloom"));
+                    }
+                }
+                NPCLoader.blockLoot.Add(ModLoader.GetMod("Tremor").ItemType("PickaxeofBloom"));
+            }
+            if (tremor != null && npc.type == tremor.NPCType("SoulofHope") && !Main.expertMode)
+            {
+                if (Main.rand.Next(3) == 0)
+                {
+                    if (Main.rand.Next(2) == 0)
+                    {
+                        Item.NewItem((int)npc.position.X, (int)npc.position.Y, npc.width, npc.height, mod.ItemType("Banjackhammer"));
+                    }
+                    else
+                    {
+                        Item.NewItem((int)npc.position.X, (int)npc.position.Y, npc.width, npc.height, ModLoader.GetMod("Tremor").ItemType("Banhammer"));
+                    }
+                }
+                NPCLoader.blockLoot.Add(ModLoader.GetMod("Tremor").ItemType("Banhammer"));
+            }
+            return true;
+        }
+    }
 }
