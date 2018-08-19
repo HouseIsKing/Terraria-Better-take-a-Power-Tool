4264e732339fc5cfed7c4a3de511f302695c985d
dennisw100 <100dennisw@gmail.com>
2018-08-20 00:34:50 +0200

    Add files via upload

--- BettertakeaPowerTool.cs
+++ BettertakeaPowerTool.cs
@@ -1,12 +1,21 @@
-using System.Collections.Generic;
+using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
+using System.Collections.Generic;
 
 namespace BettertakeaPowerTool
 {
 	class BettertakeaPowerTool : Mod
 	{
-		public static string GithubUserName { get { return "dennisw100"; } }
+        public static string ConfigFileRelativePath
+        {
+            get { return "Mod Configs/BettertakeaPowerTool.json"; }
+        }
+        public static void ReloadConfigFromFile()
+        {
+            // Define implementation to reload your mod's config data from file
+        }
+        public static string GithubUserName { get { return "dennisw100"; } }
 		public static string GithubProjectName { get { return "Terraria-Better-take-a-Power-Tool"; } }
 		public BettertakeaPowerTool()
 		{//sets propeties of the mod
@@ -17,12 +26,34 @@ namespace BettertakeaPowerTool
 				AutoloadSounds = true
 			};
 		}
-		public override void Load()
+        public override void Load()
 		{
-			List<short> list = new List<short>(Extensions.LunarTools);
+            Config.Load();
+            List<short> list = new List<short>(Extensions.LunarTools);
 			list.ForEach(x =>ItemID.Sets.Deprecated[x] = false);
 		}
-		public override void AddRecipes()
+        public override void PostAddRecipes()
+        {
+            Mod tremor = ModLoader.GetMod("Tremor");
+            if (tremor != null && Config.DommhammerjackhammerSettings == 2)
+            {
+                RecipeFinder finder = new RecipeFinder(); // make a new RecipeFinder
+                finder.AddIngredient(ItemID.Pwnhammer); // add a new recipe group, in this case the vanilla one for iron or lead bars.
+                finder.AddIngredient(tremor.ItemType("DarkBulb"), 15);
+                finder.AddIngredient(ItemID.Bone, 100);
+                finder.AddTile(TileID.MythrilAnvil); // add a required tile, any anvil
+                finder.SetResult(tremor.ItemType("Squasher")); // set the result to be 10 chains
+                Recipe exactRecipe = finder.FindExactRecipe(); // try to find the exact recipe matching our criteria
+
+                bool isRecipeFound = exactRecipe != null; // if our recipe is not null, it means we found the exact recipe
+                if (isRecipeFound) // since our recipe is found, we can continue
+                {
+                    RecipeEditor editor = new RecipeEditor(exactRecipe); // for our recipe, make a new RecipeEditor
+                    editor.DeleteRecipe(); // delete the recipe
+                }
+            }
+        }
+        public override void AddRecipes()
 		{//adds crafting recepies
 			Extensions.DestroyRecipes();
 			Extensions.MakeSolarRecipes(this);
@@ -40,7 +71,7 @@ namespace BettertakeaPowerTool
 			ItemID.NebulaDrill,
 			ItemID.VortexDrill,
 			ItemID.StardustDrill,
-		};
+        };
 		public const short LunarBar = 3467;
 		public const short AncientManipulator = 412;
 
@@ -55,11 +86,11 @@ namespace BettertakeaPowerTool
 			foreach (short lunarTool in LunarTools)
 			{
 				RecipeFinder finder = new RecipeFinder();
-				finder.SetResult(lunarTool);
+                finder.SetResult(lunarTool);
 				List<Terraria.Recipe> recipes = finder.SearchRecipes();
 				recipes.ForEach(x => { var editor = new RecipeEditor(x); editor.DeleteRecipe();});
-			}
-		}
+			} 
+        }
         public static void MakeSolarRecipes(Mod mod)
         {
             ModRecipe recipe = new ModRecipe(mod);
---
+++ Config.cs
@@ -0,0 +1,51 @@
+﻿using System.IO;
+using Terraria;
+using Terraria.IO;
+using Terraria.ModLoader;
+
+namespace BettertakeaPowerTool
+{
+    public static class Config
+    {
+        public static int DommhammerjackhammerSettings = 0;
+
+        //The file will be stored in "Terraria/ModLoader/Mod Configs/Example Mod.json"
+        static string ConfigPath = Path.Combine(Main.SavePath, "Mod Configs", "BettertakeaPowerTool.json");
+
+        static Preferences Configuration = new Preferences(ConfigPath);
+
+        public static void Load()
+        {
+            //Reading the config file
+            bool success = ReadConfig();
+
+            if (!success)
+            {
+                ErrorLogger.Log("Failed to read config file! Recreating config...");
+                CreateConfig();
+            }
+        }
+
+        //Returns "true" if the config file was found and successfully loaded.
+        static bool ReadConfig()
+        {
+            if (Configuration.Load())
+            {
+                Configuration.Get("Dommhammer/jackhammerSettings", ref DommhammerjackhammerSettings);
+                return true;
+            }
+            return false;
+        }
+
+        //Creates a config file. This will only be called if the config file doesn't exist yet or it's invalid. 
+        static void CreateConfig()
+        {
+            Configuration.Clear();
+            Configuration.Put("Dommhammer/jackhammerSettings", DommhammerjackhammerSettings);
+            Configuration.Put("Unobtainable", 0);
+            Configuration.Put("50% Drop chance to get Pwnhammer/jackhammer or Dommhammer/jackhammer from Wall of Fleash", 1);
+            Configuration.Put("Replace Pawnhammer/jackhammer and make them stronger and made with Hallow Bars", 2);
+            Configuration.Save();
+        }
+    }
+}
