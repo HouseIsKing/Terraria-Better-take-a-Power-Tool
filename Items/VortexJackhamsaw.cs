9c4d0a5f994c6070a1bce30a006b042d2ebb0abf
dennisw100 <100dennisw@gmail.com>
2018-08-27 22:25:38 +0200

    Add files via upload

--- Items/VortexJackhamsaw.cs
+++ Items/VortexJackhamsaw.cs
@@ -1,3 +1,6 @@
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
 
@@ -11,7 +14,7 @@ namespace BettertakeaPowerTool.Items
 		}
 		public override void SetDefaults()
 		{
-			item.CloneDefaults(ItemID.LunarHamaxeVortex);
+			item.CloneDefaults(ItemID.LunarHamaxeSolar);
 			item.channel = true;
 			item.noUseGraphic = true;
 			item.noMelee = true;
@@ -29,5 +32,29 @@ namespace BettertakeaPowerTool.Items
 			recipe.SetResult(this);
 			recipe.AddRecipe();
 		}
-	}
+        public override void PostDrawInWorld(SpriteBatch spriteBatch, Color lightColor, Color alphaColor, float rotation, float scale, int whoAmI)
+        {
+            Texture2D texture = mod.GetTexture("Items/VortexJackhamsaw_Glowmask");
+            spriteBatch.Draw
+            (
+                texture,
+                new Vector2
+                (
+                    item.position.X - Main.screenPosition.X + item.width * 0.5f,
+                    item.position.Y - Main.screenPosition.Y + item.height - texture.Height * 0.5f + 2f
+                ),
+                new Rectangle(0, 0, texture.Width, texture.Height),
+                Color.White,
+                rotation,
+                texture.Size() * 0.5f,
+                scale,
+                SpriteEffects.None,
+                0f
+            );
+        }
+        public override void PostUpdate()
+        {
+            Lighting.AddLight(item.Center, 0f, 0.36f, 0.4f);
+        }
+    }
 }
